35d113460770d22a01787d205a3f53ebe85abce9
bk626-hub <bk626@keeneisd.org>
2025-09-13 16:32:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-document.getElementById('dynamic-island').addEventListener('click', function() {
-    this.classList.toggle('island-expanded');
-    this.classList.toggle('island-collapsed');
-});
+
